3223683e29507cd970fc5dfcc99a5358657e8fee
Momen83 <77742840+Momen83@users.noreply.github.com>
2023-04-23 04:19:56 +0200

    Update 1416. Restore The Array.cpp

--- Dynamic_programming/1416. Restore The Array.cpp
+++ Dynamic_programming/1416. Restore The Array.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     const int Mod = 1e9 + 7;
     int dp[100001];
-    long long get_val(string &s , int st ,int en){
-        long long ans = 0;
-        for(int i = st ; i<=en ;i++){
-            ans = ans * 10 + (s[i] - '0');
-        }
-        return ans;
-    }
+  
     int solve(string &s , int start , int k) {
         if(start ==s.size()){
             return 1;
         }
-        if(s[start] =='0'){
-            return 0;
-        }
+
         int &ans = dp[start];
         if(ans !=-1)return ans;
         ans =  0;
+           long long val = 0;
         for(int end = start ; end <s.size() ; end++){
-            long long val = get_val(s , start , end) ;
-            if(val >k){
+           val = val * 10 + (s[end] - '0');
+            if(val >k || val <1){
                 break;
             }
             ans = ((ans %Mod) + (solve(s , end + 1,k)%Mod))%Mod;
