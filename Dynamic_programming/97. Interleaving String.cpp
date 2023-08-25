d393b4ff83fb6ade1ca69d54f502a379011f2ba7
Momen83 <77742840+Momen83@users.noreply.github.com>
2023-08-25 12:25:27 +0300

    Update 97. Interleaving String.cpp

--- Dynamic_programming/97. Interleaving String.cpp
+++ Dynamic_programming/97. Interleaving String.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    int dp[101][101][201];
-    int solve(string &s1 , string &s2 , string &s3 , int i , int j , int k)
+    int dp[101][101];
+    int solve(string &s1 , string &s2 , string &s3 , int i , int j)
     {
        
-        if(k == s3.size())
+        if(i + j == s3.size())
         {
-            return i == s1.size() && j== s2.size();
+            return 1;
         }
         
-        int & ans = dp[i][j][k];
+        int & ans = dp[i][j];
         if(ans !=-1) return ans;
         ans = 0;
-        if(i + 1 <=s1.size() && s3[k] == s1[i]){
-            ans |= solve(s1,s2,s3,i + 1 , j , k + 1);
+        if(i + 1 <=s1.size() && s3[i + j] == s1[i]){
+            ans |= solve(s1,s2,s3,i + 1 , j);
         }
-        if(j + 1 <= s2.size() && s3[k] == s2[j]){
-            ans |= solve(s1,s2,s3,i , j + 1 , k + 1);
+        if(j + 1 <= s2.size() && s3[i + j] == s2[j]){
+            ans |= solve(s1,s2,s3,i , j + 1);
         }
-             if(i + 1 <= s1.size())
-             {
-                  ans |= solve(s1 ,s2 ,s3 ,i+1 , j , k);
-             }
-             if(j + 1 <= s2.size())
-             {
-                  ans |= solve(s1 ,s2 ,s3 ,i , j + 1 , k);
-             }
+        
        
         return ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size()) return false;
         memset(dp,-1,sizeof(dp));
-        int ans = solve(s1,s2,s3,0,0,0);
+        int ans = solve(s1,s2,s3,0,0);
         return ans;
         
     }
